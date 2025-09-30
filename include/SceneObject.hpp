f0423d86098f6d57965079dc69767e80c05fa279
Yes Yes ye <a1939771@adelaide.edu.au>
2025-09-30 21:54:28 +0930

    Removed using namespace std; and implemented the shuffle and topCard methods in Deck.

--- include/SceneObject.hpp
+++ include/SceneObject.hpp
@@ -6,93 +6,74 @@
 #include <algorithm>
 #include <memory>
 
-using namespace std;
-
 // The card class
 class Card {
     // Two 
     private:
-    string Suit;
-    string Rank;
+    std::string suit;
+    std::string rank;
 
     public: 
     // Default Constructor
     Card()
-        : Suit(""), Rank("") // Initializer list
+        : suit(""), rank("") // Initializer list
     {}
 
-    Card(string s, string r)
-        : Suit(s), Rank(r)
+    Card(std::string s, std::string r)
+        : suit(s), rank(r)
     {}
 
     // Method to set the suit
-    void SetSuit(string s) {Suit = s;}
+    void setSuit(std::string s) {suit = s;}
     // Method to set the Rank
-    void SetRank(string r) {Rank = r;}
+    void setRank(std::string r) {rank = r;}
     // Method to read Suit 
-    string GetSuit() const {return Suit;}
+    std::string getSuit() const {return suit;}
     // Method to read Rank
-    string GetRank() const {return Rank;}
-    // Method to print card (written just in case)
-    void PrintCard() const {cout << Suit << " " << Rank;}
+    std::string getRank() const {return rank;}
 };
 
 // Equality operator for the Card class that will help when removing cards
 bool operator==(const Card& inDeck, const Card& findingCard){
-    return inDeck.GetSuit() == findingCard.GetSuit() && inDeck.GetRank() == findingCard.GetRank();
+    return inDeck.getSuit() == findingCard.getSuit() && inDeck.getRank() == findingCard.getRank();
 }
 
 class CardPool {
     protected: 
-    vector<Card> cardpool;
+    std::vector<Card> cardPool;
     public:
     // Default constructor
     CardPool() {}
     // Method for adding a card
-    void AddCard(const Card& newcard) {
-        cardpool.push_back(newcard);
+    void addCard(const Card& newCard) {
+        cardPool.push_back(newCard);
     }
     // Method for removing a card
-    void RemoveCard(const Card& removecard) {
+    void removeCard(const Card& targetCard) {
         // Find the matching card using the bool operator we defined in the Card class
-        auto TargetCard = find(cardpool.begin(), cardpool.end(), removecard);
-        if (TargetCard != cardpool.end()) {
-            cardpool.erase(TargetCard);
+        auto removingCard = std::find(cardPool.begin(), cardPool.end(), targetCard);
+        if (removingCard != cardPool.end()) {
+            cardPool.erase(removingCard);
         }
     }
     // Clear or Discard all cards
     void Clear() {
-        cardpool.clear();
-    }
-
-    // Print the cards within the vector
-    void PrintCards() const {
-        for(const auto& card : cardpool) {
-            card.PrintCard();
-            cout << " ";
-        }
-        cout << endl;
+        cardPool.clear();
     }
 };
 
 // A class that helps the scene objects' location on the screen
 class PositionalSceneObject{
     protected: 
-    double HorizontalPosition = 0.0;
-    double VerticalPosition = 0.0;
+    double horizontalPosition = 0.0;
+    double verticalPosition = 0.0;
 
     public:
-    void SetPosition(double x, double y) {
-        HorizontalPosition = x;
-        VerticalPosition = y;
-    }
-
-    void SetHorizontalPosition(double x) {
-        HorizontalPosition = x;
-    }
-
-    void SetVerticalPosition(double y) {
-        VerticalPosition = y;
+    void setPosition(double x, double y) {
+        if(x > 0 && x < 1 && y > 0 && y < 1){
+            horizontalPosition = x;
+            verticalPosition = y;
+        }
     }
 };
 
@@ -100,48 +81,59 @@ class PositionalSceneObject{
 class Deck : public CardPool, public PositionalSceneObject {
     private: 
     // Building vectors for the card types 
-    static const vector<string> Suits;
-    static const vector<string> Ranks;
+    static const std::vector<std::string> SUITS;
+    static const std::vector<std::string> RANKS;
     public:
     // Method for Building decks
-    void BuildDeck() {
+    void buildDeck() {
         // Reset Deck
         Clear();
         // Two for loops to go through every pairing
-        for(const string& s : Suits){
-            for(const string& r : Ranks){
-                AddCard(Card(s, r));
+        for(const std::string& s : SUITS){
+            for(const std::string& r : RANKS){
+                addCard(Card(s, r));
             }
         }
     }
     // Method for adding joker 
-    void AddJokers() {
-        AddCard(Card("Coloured", "Joker"));
-        AddCard(Card("NotColoured", "Joker"));
+    void addJokers() {
+        addCard(Card("Coloured", "Joker"));
+        addCard(Card("NotColoured", "Joker"));
+    }
+    // Return the top card of the vector 
+    Card topCard() {
+        if(cardPool.size() != 0){
+            return cardPool.back();
+        }
+    }
+    // Shuffle the vector
+    void shuffle() {
+        auto rng = std::default_random_engine {};
+        std::shuffle(std::begin(cardPool), std::end(cardPool), rng);
     }
 };
 
-const vector<string> Deck::Suits = {"H", "D", "C","S"};
-const vector<string> Deck::Ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+const std::vector<std::string> Deck::SUITS = {"H", "D", "C","S"};
+const std::vector<std::string> Deck::RANKS = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
 
 class Hand : public CardPool, public PositionalSceneObject {
 };
 
 class Text : public PositionalSceneObject {
     private: 
-    vector<string> ScreenText; 
+    std::vector<std::string> screenText; 
     public: 
-    void AddText(string newText) {
-        ScreenText.push_back(newText);
+    void addText(std::string newText) {
+        screenText.push_back(newText);
     }
 };
 
 class SceneObject {
     private:
     // Use smart pointers for better memory allocation
-    vector<unique_ptr<PositionalSceneObject>> Objects;
+    std::vector<std::unique_ptr<PositionalSceneObject>> objects;
     public:
-    void AddObject(unique_ptr<PositionalSceneObject> NewObject) {
-        Objects.push_back(move(NewObject));
+    void addObject(std::unique_ptr<PositionalSceneObject> newObject) {
+        objects.push_back(std::move(newObject));
     }
 };
